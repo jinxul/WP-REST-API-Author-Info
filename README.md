64ce60916350df0560d93f978164385ab255c8a2
ahmad Givekesh <ahmadgivekesh@gmail.com>
2016-03-26 17:58:57 +0430

    initial release.

---
+++ README.md
@@ -0,0 +1,35 @@
+WP REST API - Author Info Plugin
+=================
+
+This plugin will get Author's Display Name for each Post in WP REST API.
+
+It takes this:
+
+
+	author: 1,
+
+
+And turns it into this:
+
+
+	author_info: {
+		id: 1,
+		display_name: "AUTHOR_DISPLAY_NAME"
+	},
+
+
+License
+=======
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses
---
+++ wp-rest-api-author-info.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: WP REST API - Author Info
+Description: Getting Author's Display Name for each Post in REST API.
+Version:     1.0
+Author:      Ahmad Givekesh
+Author URI:  baboon.ir
+License:     GPL v3.0
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+
+add_action( 'init', 'author_info_init', 12);
+
+function author_info_init() {
+
+	$post_types = get_post_types( array( 'public' => true ), 'objects' );
+
+	foreach ( $post_types as $post_type ) {
+
+		$post_type_name     = $post_type->name;
+		$show_in_rest       = ( isset( $post_type->show_in_rest ) && $post_type->show_in_rest ) ? true : false;
+
+	
+		if ( $show_in_rest ) {
+
+			if ( function_exists( 'register_rest_field' ) ) {
+				register_rest_field( $post_type_name,
+					'author_info',
+					array(
+						'get_callback' => 'author_info_get_field',
+						'schema'       => null,
+					)
+				);
+			} elseif ( function_exists( 'register_api_field' ) ) {
+				register_api_field( $post_type_name,
+					'author_info',
+					array(
+						'get_callback' => 'author_info_get_field',
+						'schema'       => null,
+					)
+				);
+			}
+		}
+	}
+}
+
+function author_info_get_field( $object, $field_name ) {
+
+	if ( ! empty( $object['author'] ) ) {
+		$author_id = (int)$object['author'];
+	} else {
+		return null;
+	}
+
+	$author['id']           	= $author_id;
+	$author['display_name']		= get_the_author_meta('display_name', $author_id);
+	
+	
+	return apply_filters( 'author_info', $author);
+}
